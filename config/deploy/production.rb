60369c109bdeeed279f11be0fa42241ff66517ae
Ian Wilson <me@ianwilson.org>
2015-08-29 00:07:28 -0400

    Moar Deployment....

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -15,7 +15,6 @@ role :db,  %w{deploy@ptdr.iwcg.net}
 # definition into the server list. The second argument
 # something that quacks like a hash can be used to set
 # extended properties on the server.
-server 'example.com', user: 'deploy', roles: %w{web app}, my_property: :my_value
 
 # you can set custom ssh options
 # it's possible to pass any option but you need to keep in mind that net/ssh understand limited list of options
