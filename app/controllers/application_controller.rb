2b49fc2d7ad391f1479c173ef80da78bbb2826a4
Ian Wilson <me@ianwilson.org>
2015-08-29 03:12:31 -0400

    You should actually see your name in the dropdown.  I really should write a test that covers this....

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,16 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+  before_action :configure_permitted_params, if: :devise_controller?
   protect_from_forgery with: :exception
+
+
+  protected
+
+  def configure_permitted_params
+    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:name, :email) }
+    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:name, :email, :current_password) }
+  end
+
+
 end
